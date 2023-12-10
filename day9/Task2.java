d25e663e984202c03b9b8ec7af44e94353e4f6bc
Henrik Hagfeldt <henke9606@hotmail.com>
2023-12-10 01:20:33 +0100

    Day 9

---
+++ day9/Task2.java
@@ -0,0 +1,48 @@
+package day9;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static utils.Parser.readLinesOfFile;
+import static utils.Printer.print;
+
+public class Task2 {
+    public static void main(String[] args) throws IOException {
+        List<String> histories = readLinesOfFile("day9/data.input");
+
+        long totalSum = 0;
+        for(String history : histories) {
+            String[] historyArray = history.split(" ");
+            List<Integer> historyList = new ArrayList<>();
+            for(String s : historyArray) {
+                historyList.add(Integer.valueOf(s));
+            }
+
+            int value = digDeeper(historyList);
+            print("Value is: %s", value);
+            totalSum += value;
+        }
+
+        print("Total steps: %s", totalSum);
+    }
+
+    private static Integer digDeeper(List<Integer> currentLevel) {
+
+        if (currentLevel.stream().allMatch(x -> x == 0)) {
+            return 0;
+        }
+
+        List<Integer> differences = new ArrayList<>();
+        for (int i = 0; i < currentLevel.size() - 1; i++) {
+            int value = currentLevel.get(i);
+            int nextValue = currentLevel.get(i + 1);
+            int difference = nextValue - value;
+            differences.add(difference);
+        }
+
+        print("Differences %s", differences);
+        return  currentLevel.get(0) - digDeeper(differences);
+
+    }
+}
